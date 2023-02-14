3e009255f9e85988586448f01cc280ebc8434182
Jurgen <s1133569@student.windesheim.nl>
2023-02-14 18:37:24 +0100

    Initial commit

---
+++ app/src/main/java/com/company/mathgame/Subtraction.java
@@ -0,0 +1,151 @@
+package com.company.mathgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+import java.util.Random;
+
+public class Subtraction extends AppCompatActivity {
+
+    TextView score;
+    TextView life;
+    TextView time;
+
+    TextView question;
+    EditText answer;
+
+    Button ok;
+    Button next;
+    Random random = new Random();
+    int number1;
+    int number2;
+    int userAnswer;
+    int realAnswer;
+    int userScore = 0;
+    int userLife = 3;
+
+    CountDownTimer timer;
+    private static final long START_TIMER_IN_MILIS = 10000;
+    Boolean timer_running;
+    long time_left_in_milis = START_TIMER_IN_MILIS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        score = findViewById(R.id.textViewScore);
+        life = findViewById(R.id.textViewLife);
+        time = findViewById(R.id.textViewTime);
+        question = findViewById(R.id.textViewQuestion);
+        answer = findViewById(R.id.editTextAnswer);
+        ok = findViewById(R.id.buttonOke);
+        next = findViewById(R.id.buttonNext);
+
+        gameContinue();
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+
+                userAnswer = Integer.parseInt(answer.getText().toString());
+
+                pauseTimer();
+
+                if( userAnswer == realAnswer){
+                    userScore = userScore + 10;
+                    score.setText(""+userScore);
+                    question.setText("congrats its correct");
+                }else{
+                    userLife = userLife -1;
+                    life.setText(""+userLife);
+                    question.setText("uncorrect");
+                }
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answer.setText("");
+                resetTimer();
+
+                if (userLife ==  0){
+                    Toast.makeText(getApplicationContext(), "game over", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Subtraction.this, Result.class);
+                    intent.putExtra("score", userScore);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    gameContinue();
+                }
+            }
+        });
+    }
+
+    public void gameContinue(){
+        number1 = random.nextInt(100);
+        number2 = random.nextInt(100);
+
+        if( number1 < number2){
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+        }
+        realAnswer = number1 - number2;
+
+        question.setText(number1 + " - " + number2);
+        startTimer();
+    }
+
+    public void startTimer()
+    {
+        timer = new CountDownTimer(time_left_in_milis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                time_left_in_milis = millisUntilFinished;
+                updateText();
+            }
+
+            @Override
+            public void onFinish() {
+                timer_running = false;
+                pauseTimer();
+                resetTimer();
+                updateText();
+                userLife = userLife -1;
+                life.setText(""+userLife);
+                question.setText("to slow times up");
+            }
+        }.start();
+
+        timer_running = true;
+    }
+
+    public void updateText(){
+        int second = (int)(time_left_in_milis / 1000) %60;
+        String time_left = String.format(Locale.getDefault(), "%02d", second);
+        time.setText(time_left);
+    }
+
+    public void pauseTimer(){
+        timer.cancel();
+        timer_running = false;
+    }
+
+    public void resetTimer(){
+        time_left_in_milis = START_TIMER_IN_MILIS;
+        updateText();
+    }
+}
